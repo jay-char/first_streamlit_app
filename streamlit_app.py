c0fe26b264952828339f88de239dc1a48655c59f
Tamonwan (Jay) Charoensripongsa <116792534+jay-char@users.noreply.github.com>
2023-07-13 15:46:51 +1000

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,3 +1,8 @@
 import streamlit
 
 streamlit.title('My Parents New Healthy Diner')
+
+streamlit.header('Breakfast Menu')
+streamlit.text('Omega 3 & Bluebuerry Oatmeal')
+streamlit.text('Kale, Spinach & Rocket Smoothie')
+streamlit.text('Hard-Boiled Free_range Egg')
